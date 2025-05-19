59e1a5267a1d41d6ac2abc48f17aef8d81c0dd6a
Kim-ilhyeon <kih3915@naver.com>
2025-05-19 10:16:11 +0900

    frontend 기본 폴더구조 및 reset.css적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/styles/reset.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      hello
+      <div style={{border: 'solid 1px red', width: '2rem', height: '3rem'}}>
+
+      </div>
     </div>
   );
 }
