81a2a5db1a0f1ba427188fb7f5c8f66d4bc96357
vocoxo9 <vocoxo9@gmail.com>
2025-05-20 18:15:19 +0900

    운동추천-반복일설정페이지

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Header from 'components/common/header/Header';
 import Footer from 'components/common/footer/Footer';
+import ExerciseRepeatsDay from 'pages/exercise/selectRepeatsDay/ExerciseRepeatsDay';
+import ExerciseInputInfo from 'pages/exercise/inputInformation/ExerciseInputInfo';
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
         <Header />
 
         <div className='layout'>
-          
+          <ExerciseInputInfo />
+          <ExerciseRepeatsDay />
         </div>
 
         <Footer />
