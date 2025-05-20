a5e579b3dedba7fb706a8823f5d7fb5636ad193a
Tokemo <similes_pax_9h@icloud.com>
2025-05-20 17:09:03 +0900

    Radio 컴포넌트 수정, RadioGroup 컴포넌트 추가

--- src/components/common/Radio.jsx
+++ src/components/common/Radio.jsx
@@ -1,12 +1,42 @@
-import '../css/Radio.css';
+import styles from './Radio.module.css'
 
-
-function Radio({id, text, name}) {
+/**
+ * 라디오 컴포넌트.
+ *
+ * @param id radio 태그의 id
+ * @param name radio 태그의 name
+ * @param value radio 태그의 value
+ * @param label radio 태그에 대한 label 텍스트
+ * @param [checked] radio 태그의 checked (선택)
+ * @param [onChange] radio 태그의 onChange (선택)
+ * @returns {JSX.Element} 라디오 컴포넌트
+ */
+const Radio = (
+    {
+        id,
+        name,
+        value,
+        label,
+        checked,
+        onChange
+    }
+) => {
     return (
-        <div className='radio'>
-            <label for={id}>{text}</label>
-            <input type="radio" id={id} name={name} />
+        <div className={styles.container}>
+            <input
+                className={styles.input}
+                type="radio"
+                id={id}
+                name={name}
+                value={value}
+                checked={checked}
+                onChange={onChange}
+            />
+            <label className={styles.label} htmlFor={id}>
+                {label}
+            </label>
         </div>
-    );
+    )
 }
-export default Radio;
+
+export default Radio
