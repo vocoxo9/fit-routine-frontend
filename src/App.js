8aea38a563648bc9963b593929d8f6184eaeb9e5
vocoxo9 <vocoxo9@gmail.com>
2025-05-20 16:18:02 +0900

    운동추천-정보입력페이지

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from 'components/common/header/Header';
 import Footer from 'components/common/footer/Footer';
 import ErrorPage from 'pages/common/errorPage/ErrorPage';
+import ExerciseInputInfo from 'pages/exercise/inputInformation/ExerciseInputInfo';
 
 function App() {
   return (
@@ -13,8 +14,7 @@ function App() {
         <Header />
 
         <div className='layout'>
-
-          
+          <ExerciseInputInfo />
         </div>
 
         <Footer />
