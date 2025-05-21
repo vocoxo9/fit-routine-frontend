b1957e6d0098f30930381860c6dbf1bc9a5302ff
Tokemo <similes_pax_9h@icloud.com>
2025-05-21 15:52:38 +0900

    CheckBox 완성

--- src/components/common/checkBox/CheckBox.jsx
+++ src/components/common/checkBox/CheckBox.jsx
@@ -1,12 +1,42 @@
-import '../css/CheckBox.css';
+import styles from './CheckBox.module.css'
 
-function CheckBox({name, text}) {
+/**
+ * 체크박스 컴포넌트.
+ *
+ * @param id checkbox 태그의 id
+ * @param name checkbox 태그의 name
+ * @param value checkbox 태그의 value
+ * @param label checkbox 태그에 대한 label 텍스트
+ * @param [checked] checkbox 태그의 checked (선택)
+ * @param [onChange] checkbox 태그의 onChange (선택)
+ * @returns {JSX.Element} 체크박스 컴포넌트
+ */
+const CheckBox = (
+    {
+        id,
+        name,
+        value,
+        label,
+        checked,
+        onChange
+    }
+) => {
     return (
-        <div className='checkBox'>
-            <label htmlFor={text}>{text}</label>
-            <input type="checkbox" name={name} id={text} />
+        <div className={styles.container}>
+            <input
+                className={styles.input}
+                type="checkbox"
+                id={id}
+                name={name}
+                value={value}
+                checked={checked}
+                onChange={onChange}
+            />
+            <label className={styles.label} htmlFor={id}>
+                {label}
+            </label>
         </div>
-    );
+    )
 }
 
-export default CheckBox;
+export default CheckBox
