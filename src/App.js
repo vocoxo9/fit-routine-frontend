2ef7085efcdf4c14d57c6df3c721ec365b183219
vocoxo9 <vocoxo9@gmail.com>
2025-05-22 17:26:25 +0900

    운동추천 페이지 루틴 추천페이지 ui 구현 - app.js 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {BrowserRouter} from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
-import RecommendRoutine from 'pages/recommend/exercise/RecommendRoutine/RecommendRoutine';
 
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
         <Header />
 
         <div className={styles.layout}>
-            <RecommendRoutine />
+
         </div>
 
         <Footer />
