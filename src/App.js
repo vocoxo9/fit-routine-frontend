6bb7e85845da90ff288511aeef69545ee5380dff
seongjae <vhrvkflrur@naver.com>
2025-05-26 01:05:15 +0900

    todolist의 식단,운동 리스트 컴포넌트로 분리

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
-import AllBoardsPage from 'pages/blog/AllBoardsPage/AllBoardsPage';
-import BoardDetail from 'pages/blog/BoardDetailPage/BoardDetail';
-import FormContainer from 'components/common/FormContainer/FormContainer';
-import LikeList from 'components/member/mypage/likeList/LikeList';
-import Profile from 'components/member/mypage/profile/Profile';
-import OnesBlogPage from 'pages/blog/OnesBlogPage/OnesBlogPage';
-import OnesTodoPage from 'pages/blog/OnesTodoPage/OnesTodoPage';
 
 function App() {
   return (
@@ -19,7 +12,7 @@ function App() {
         <Header />
 
         <div className={styles.layout}>
-          <OnesTodoPage/>
+          
         </div>
 
         <Footer />
