059db7fbe4772cba1bedb241842e77c163b345fd
seongjae213 <112374455+seongjae213@users.noreply.github.com>
2025-05-26 17:07:33 +0900

    Update MessageForm.jsx

--- src/components/recommend/MessageForm/MessageForm.jsx
+++ src/components/recommend/MessageForm/MessageForm.jsx
@@ -13,8 +13,12 @@ const MessageForm = (
     return (
         <>
             <div className={styles.message}>
-                <p>선택하신 운동의 총 소모 칼로리는 <span>{data.kcal}</span>kcal입니다.</p>
-                <p>{data.kcal}/{data.burnKcal}</p>
+                <p className={styles.kcal}>
+                    선택하신 운동의 총 소모 칼로리는 <span>{data.kcal}</span>kcal입니다.
+                </p>
+                <p className={styles.burnKcal}>
+                    {data.kcal}/{data.burnKcal}
+                </p>
             </div>
         </>
     );
