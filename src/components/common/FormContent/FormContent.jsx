a56854775cc6a8a078b69167229ed3b9b542580e
Tokemo <similes_pax_9h@icloud.com>
2025-05-29 14:04:34 +0900

    form.module.css 추가 및 FormContainer, FormContent, FormTitle 제거

--- src/components/common/FormContent/FormContent.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './FormContent.module.css';
-
-const FormContent = ({ children }) => {
-    return <div className={styles.formBox}>{children}</div>;
-};
-
-export default FormContent;
