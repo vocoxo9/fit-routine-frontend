06e7d15bebe0b7c46d6ae0db1013df7d9a5320da
Kim-ilhyeon <kih3915@naver.com>
2025-05-29 17:16:32 +0900

    revert App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import reset from './assets/styles/common/Reset.module.css';
+import styles from './assets/styles/common/Reset.module.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
+import OnesTodoPage from 'pages/blog/OnesTodoPage/OnesTodoPage';
+import BoardAddEditPage from 'pages/blog/BoardAddEditPage/BoardAddEditPage';
+import BoardDetail from 'pages/blog/BoardDetailPage/BoardDetail';
+import OnesBlogPage from 'pages/blog/OnesBlogPage/OnesBlogPage';
+import AllBoardsPage from 'pages/blog/AllBoardsPage/AllBoardsPage';
 
 function App() {
     return (
@@ -11,12 +16,13 @@ function App() {
             <BrowserRouter>
                 <Header />
 
-                <div className={reset.layout}></div>
+                <div className={styles.layout}>
+                    <BoardDetail/>
+                </div>
 
                 <Footer />
             </BrowserRouter>
         </>
     );
 }
-
 export default App;
