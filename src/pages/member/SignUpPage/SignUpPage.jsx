bd36257626058b83610ef98ff2392accbffadfbb
Tokemo <similes_pax_9h@icloud.com>
2025-05-29 22:21:23 +0900

    태그 변화에 따른 SignUpForm 및 SignUpPage 수정

--- src/pages/member/SignUpPage/SignUpPage.jsx
+++ src/pages/member/SignUpPage/SignUpPage.jsx
@@ -1,13 +1,10 @@
-import styles from './SignUpPage.module.css';
+import styles from './SignUpPage.module.css'
 import SignUpForm from 'components/member/SignUpForm/SignUpForm';
 
 const SignUpPage = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.title}>가입 페이지</div>
-            <div className={styles.content}>
-                <SignUpForm />
-            </div>
+            <SignUpForm />
         </div>
     );
 };
