c57659fc49bea06f304101e910bd29e0a31ee648
vocoxo9 <vocoxo9@gmail.com>
2025-05-30 21:05:19 +0900

    label-input간의 gap 수정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
+import MainPage from 'pages/main/MainPage/MainPage';
+import SignUpForm from 'components/member/SignUpForm/SignUpForm';
+import SignUpPage from 'pages/member/SignUpPage/SignUpPage';
+import RecommendExercise from 'pages/recommend/exercise/RecommendExercise/RecommendExercise';
+import RecommendForm from 'components/recommend/RecommendForm/RecommendForm';
+import MyPage from 'pages/member/MyPage/MyPage';
 
 function App() {
     return (
@@ -12,6 +18,10 @@ function App() {
                 <Header />
 
                 <div className={styles.layout}>
+                    <MainPage/>
+                    <SignUpForm/>
+                    <RecommendForm/>
+                    <MyPage/>
                 </div>
 
                 <Footer />
