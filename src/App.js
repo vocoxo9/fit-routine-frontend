7cef8827562c670cc433594e6246ab255c91c09a
vocoxo9 <vocoxo9@gmail.com>
2025-05-30 14:51:52 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
-import ExerciseInputInfo from 'pages/recommend/exercise/ExerciseInputInfo/ExerciseInputInfo';
-import ExerciseRepeatsDay from 'pages/recommend/exercise/ExerciseRepeatsDay/ExerciseRepeatsDay';
-import RecommendExercise from 'pages/recommend/exercise/RecommendExercise/RecommendExercise';
-import RecommendForm from 'components/recommend/RecommendForm/RecommendForm';
 
 function App() {
     return (
@@ -16,9 +12,6 @@ function App() {
                 <Header />
 
                 <div className={styles.layout}>
-                    {/* <ExerciseInputInfo/> */}
-                    {/* <ExerciseRepeatsDay/> */}
-                    <RecommendExercise/>
                 </div>
 
                 <Footer />
