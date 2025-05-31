c08301e0d31f115193eab4817893fffc25359a6f
seongjae <vhrvkflrur@naver.com>
2025-06-01 06:07:35 +0900

    상세 페이지 브랜치 기반 작업 브랜치 이동

--- src/pages/blog/OnesBlogPage/OnesBlogPage.jsx
+++ src/pages/blog/OnesBlogPage/OnesBlogPage.jsx
@@ -1,64 +1,72 @@
 import styles from './OnesBlogPage.module.css';
-import { VscEdit } from 'react-icons/vsc';
 import BoardsPaging from 'components/blog/BoardsPaging/BoardsPaging';
 import Likes from 'components/common/Likes/Likes';
 import BlogGrade from 'components/common/BlogGrade/BlogGrade';
 import GenderImage from 'components/common/GenderImage/GenderImage';
-import buttons from 'assets/styles/common/button.module.css';
-import { useEffect, useParams } from 'react';
+import { useEffect, useParams, useState } from 'react';
+import Introduce from 'components/blog/Introduce/Introduce';
 
 /**
  * 블로그 페이지
  */
 function OnesBlogPage() {
-    // const { nickname } = useParams('일김현');    // <Route path="/blog/onesblog/:nickname" element={<OnesBlogPage />} />
+    // const { nickname } = useParams('');    // <Route path="/blog/onesblog/:nickname" element={<OnesBlogPage />} />
+    const [blog, setBlog] = useState({});
+    const [blogLike, setBlogLike] = useState({});
+    
+    
 
-    // nickname으로 blog정보 api요청
+    // nickname과 로그인유저의 토큰(좋아요 확인용)으로 blog정보 api요청
     const blogDetail = async () => {
-        // axios.get('blog/onesblog?nickname='+nickname);
+        // axios.get('blog/onesblog?nickname='+nickname, {token});
+
+        // 더미데이터
+        const data = {
+            nickname:'기밀현',
+            gender:'male',
+            introduce:'안녕하세요. 제 소개글을 봐주셔서 감사합니다.근데만약이렇게까지길어진다면어쩔건데\n저는 김일현이구요.\n너무 피곤하네요.\n\n\n어우우',
+            like:{
+                likeCount:16,
+                isLiked:false,
+            },
+            blogGrade:87,
+        }
+        setBlog({
+            nickname:data.nickname,
+            gender:data.gender,
+            introduce:data.introduce,
+            blogGrade:data.blogGrade,
+        })
+        setBlogLike(data.like);
     };
 
     useEffect(() => {
         blogDetail();
     }, []);
 
+    
+
     return (
         <div className={styles.blogContainer}>
             <div className={styles.blogHeader}>
                 <div className={styles.ownerCard}>
                     <div className={styles.cardHeader}>
-                        <GenderImage gender="female" />
-                        <div className={styles.ownerName}>일김현's Blog</div>
+                        <GenderImage gender={blog.gender} />
+                        <div className={styles.ownerName}>{blog.nickname}'s Blog</div>
                         <div className={styles.follow}>
-                            <Likes count={46437} isBig={true} />
+                            <Likes count={blogLike.likeCount} isBig={true} isLiked={blogLike.isLiked} />
                         </div>
                     </div>
                     <hr></hr>
-                    <div className={styles.introduceContainer}>
-                        <div className={styles.introduceHeader}>
-                            <button className={`${buttons.button} ${styles.editBtn}`}>
-                                <VscEdit />
-                            </button>
-                        </div>
-                        <div className={styles.introduceText}>
-                            {'안녕하세요.\n\n제 이름은 김일현입니다.\n잘 부탁드립니다.'
-                                .split('\n')
-                                .map((line, idx) => (
-                                    <span key={idx}>
-                                        {line}
-                                        <br />
-                                    </span>
-                                ))}
-                        </div>
-                    </div>
+                    <Introduce intro={blog.introduce}/>
                 </div>
                 <div className={styles.gradeContainer}>
-                    <BlogGrade grade={359} />
+                    <BlogGrade grade={blog.blogGrade} />
                 </div>
             </div>
 
             <div className={styles.boardsContainer}>
-                <BoardsPaging />
+                <BoardsPaging nickname={blog.nickname}/>
             </div>
         </div>
     );
