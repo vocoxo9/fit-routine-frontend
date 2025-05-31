9e190a155fab6f3622d2d0cae49b9cad32e89c8f
Kim-ilhyeon <kih3915@naver.com>
2025-05-31 21:24:37 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
-import MainPage from 'pages/main/MainPage/MainPage';
-import SignUpForm from 'components/member/SignUpForm/SignUpForm';
-import SignUpPage from 'pages/member/SignUpPage/SignUpPage';
-import RecommendExercise from 'pages/recommend/exercise/RecommendExercise/RecommendExercise';
-import RecommendForm from 'components/recommend/RecommendForm/RecommendForm';
-import MyPage from 'pages/member/MyPage/MyPage';
 
 function App() {
     return (
@@ -18,10 +12,6 @@ function App() {
                 <Header />
 
                 <div className={styles.layout}>
-                    <MainPage/>
-                    <SignUpForm/>
-                    <RecommendForm/>
-                    <MyPage/>
                 </div>
 
                 <Footer />
