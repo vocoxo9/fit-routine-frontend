203190f16bdf4158d6ea438fbcb93270996bc6b3
seongjae213 <112374455+seongjae213@users.noreply.github.com>
2025-06-01 05:27:16 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from 'components/layout/Header/Header';
 import Footer from 'components/layout/Footer/Footer';
-import BoardDetail from 'pages/blog/BoardDetailPage/BoardDetail';
 
 function App() {
     return (
@@ -13,7 +12,6 @@ function App() {
                 <Header />
 
                 <div className={styles.layout}>
-                    <BoardDetail/>
                 </div>
 
                 <Footer />
