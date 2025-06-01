d7ddc0cfdaadc457ed79b67dc2ae43ffe64cd5c4
Kim-ilhyeon <kih3915@naver.com>
2025-06-02 00:41:39 +0900

    BarChart 로딩중 처리 추가

--- src/components/common/BarChart/BarChart.jsx
+++ src/components/common/BarChart/BarChart.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import styles from './BarChart.module.css';
 import { getRoutineMvpUser } from 'utils/api/mainApi.js';
 import { Bar } from 'react-chartjs-2';
 import {
@@ -21,26 +22,39 @@ ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip);
  * @param {string[]} [props.colors] - 각 항목의 색상
  */
 function BarChart() {
+    // MVP 3인의 데이터
     const [mvpData, setMvpData] = useState(
         {won: '', second: '', third: ''}
     );
+    // 로딩 상태
+    const [loading, setLoading] = useState(true);
 
     useEffect( () => {
         const fetchMvpData = async () => {
-            // 루틴 테이블에서 아이디 별로 그룹화한 것 중에 수가 가장 많은
-            // 3개의 아이디를 구해서 해당 회원의 닉네임 가져오는 함수
-            const result = await getRoutineMvpUser();
-            alert(result);
-            setMvpData (result);
-            
-            // 임시 처리
-            setMvpData(
-                {won: '다이어트는 내일부터', second: '홍길동', third: '내가 임마'}
-            );
+            try {
+                // 루틴 테이블에서 아이디 별로 그룹화한 것 중에 수가 가장 많은
+                // 3개의 아이디를 구해서 해당 회원의 닉네임 가져오는 함수
+                const result = await getRoutineMvpUser();
+                alert(result);
+                setMvpData (result);
+                
+                // 임시 처리
+                setMvpData(
+                    {won: '다이어트는 내일부터', second: '홍길동', third: '내가 임마'}
+                );
+            } catch (error) {
+                console.error("MVP 데이터 가져오기 실패", error);
+            } finally {
+                setLoading(false);
+            }
         };
 
         fetchMvpData();
     }, []);
+
+    if (loading) {
+        return <p className={styles.loadingMsg}>차트 생성 중...</p>;
+    }
 
     const data = {
         labels: [mvpData.won, mvpData.second, mvpData.third],
