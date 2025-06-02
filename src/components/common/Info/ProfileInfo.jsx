9af775d30c92303d7ad92757adacf7156e528a2e
Kim-ilhyeon <kih3915@naver.com>
2025-06-02 19:37:41 +0900

    ProfileInfo컴포넌트에서 사용하는 icon 컴포넌트화

--- src/components/common/Info/ProfileInfo.jsx
+++ src/components/common/Info/ProfileInfo.jsx
@@ -7,35 +7,9 @@ import { LiaRulerVerticalSolid } from 'react-icons/lia';
 import { IoScaleOutline } from 'react-icons/io5';
 
 import styles from './ProfileInfo.module.css';
+import InfoIcon from './InfoIcon/InfoIcon';
 
 function ProfileInfo({ kind, text, info }) {
-    let icon;
-
-    switch (kind) {
-        case 'email':
-            icon = <TfiEmail size="1.32rem" />;
-            break;
-        case 'nickName':
-            icon = <FiUser size="1.32rem" />;
-            break;
-        case 'birth':
-            icon = <BsCake2 size="1.32rem" />;
-            break;
-        case 'phone':
-            icon = <IoIosPhonePortrait size="1.32rem" />;
-            break;
-        case 'gender':
-            icon = <BsGenderAmbiguous size="1.32rem" />;
-            break;
-        case 'height':
-            icon = <LiaRulerVerticalSolid size="1.32rem" />;
-            break;
-        case 'weight':
-            icon = <IoScaleOutline size="1.32rem" />;
-            break;
-        default:
-            icon = null;
-    }
 
     return (
         <>
@@ -45,7 +19,7 @@ function ProfileInfo({ kind, text, info }) {
                 </div>
                 <div className={styles.info}>
                     <div className={styles.infoText}>
-                        {icon}
+                        <InfoIcon kind={kind} />
                         <div className={styles.text}>|</div>
                         {info}
                     </div>
