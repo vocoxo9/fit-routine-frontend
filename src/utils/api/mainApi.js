02c88c972ea0be0b63a35d898c25acff7c32c13f
Kim-ilhyeon <kih3915@naver.com>
2025-06-04 14:46:51 +0900

    mainpage-popular-board api요청 추가

--- src/utils/api/mainApi.js
+++ src/utils/api/mainApi.js
@@ -1,3 +1,15 @@
+import axios from "axios";
+
+const apiAxios = axios.create({
+    baseURL: 'http://localhost:8080',
+    withCredentials: true // 쿠키 또는 인증 헤더를 포함하여 요청할 것인지에 대한 설정
+});
+
+const getPopularBoardtop3 = async () => {
+    const response = await apiAxios.get('/board/popular');
+    return response.data;
+}
+
 // Routine을 이번달 가장 많이 등록한 회원 3명을 조회하는 api 함수
 const getRoutineMvpUser = async () => {
     // api 함수 : TODO
@@ -13,4 +25,5 @@ const getRoutineMvpUser = async () => {
 
 export {
     getRoutineMvpUser,
+    getPopularBoardtop3,
 };
