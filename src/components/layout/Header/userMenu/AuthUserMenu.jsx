f1f473191b2b42d4e0b5937777b34fa0dd1d018d
Kim-ilhyeon <kih3915@naver.com>
2025-06-12 12:02:59 +0900

    헤더 루틴 자동 스크롤 구현

--- src/components/layout/Header/userMenu/AuthUserMenu.jsx
+++ src/components/layout/Header/userMenu/AuthUserMenu.jsx
@@ -15,23 +15,49 @@ export default function AuthUserMenu() {
     return (
         <div className={styles.headerRight}>
             <div className={styles.icons}>
-                <Link to="mypage">
-                    <FaUser className={styles.icon} />
-                </Link>
-                <div>
-                    <FaBell
-                        className={styles.icon}
-                        onClick={handleNOtificationOpen}
-                    />
-                    <span className={styles.notification}>33</span>
-                    <div
-                        className={`${styles.bellNotifications} ${isOpen ? '' : styles.hide}`}>
-                        <BellNotifications />
+                <div className={styles.iconsArea}>
+                    <Link to="mypage">
+                        <FaUser className={styles.icon} />
+                    </Link>
+                    <div>
+                        <FaBell
+                            className={styles.icon}
+                            onClick={handleNOtificationOpen}
+                            />
+                        <span className={styles.notification}>33</span>
+                        <div
+                            className={`${styles.bellNotifications} ${isOpen ? '' : styles.hide}`}>
+                            <BellNotifications />
+                        </div>
                     </div>
                 </div>
                 <button className={styles.button}>로그아웃</button>
             </div>
-            <div style={{border: 'solid 1px #D9D9D9', padding: '0.5rem'}}>운동 - 바벨로우</div>
+            <div className={styles.carousel}>
+                <div className={styles.track}>
+                    <div className={styles.group}>
+                        <div className={styles.category}>
+                            <div className={styles.card}>운동 - 바벨로우</div>
+                            <div className={styles.card}>운동 - 시티드로우</div>
+                        </div>
+                        <div className={styles.category}>
+                            <div className={styles.card}>식단 - 김치찌개</div>
+                            <div className={styles.card}>식단 - 시금치</div>
+                        </div>
+                    </div>
+                    {/* 위와 동일한 카드 목록 복제 */}
+                    {/* <div className={styles.group}>
+                        <div className={styles.category}>
+                            <div className={styles.card}>운동 - 바벨로우</div>
+                            <div className={styles.card}>운동 - 시티드로우</div>
+                        </div>
+                        <div className={styles.category}>
+                            <div className={styles.card}>식단 - 김치찌개</div>
+                            <div className={styles.card}>식단 - 시금치</div>
+                        </div>
+                    </div> */}
+                </div>
+            </div>
         </div>
     );
 }
