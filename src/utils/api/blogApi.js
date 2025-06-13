56f8a277733e8ee2692787834c8f7904d68ad594
seongjae213 <vhrvkflrur@naver.com>
2025-06-13 11:42:45 +0900

    백엔드 리팩토링 맞춰서 수정

--- src/utils/api/blogApi.js
+++ src/utils/api/blogApi.js
@@ -4,47 +4,56 @@ const apiAxios = axios.create({
     baseURL: 'http://localhost:8080',
 });
 
-apiAxios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token'); 
+// apiAxios.interceptors.request.use(
+//   (config) => {
+//     // const token = localStorage.getItem('token'); 
+//     const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0XzFAZ21haWwuY29tIiwiaWF0IjoxNzQ5NzE4MzI4LCJleHAiOjE3NDk4MDQ3Mjh9.9z1rIutjbzrdB2L9vHosmLRhjdak_75erD3I-N7ke4E';
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
 
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 
 const getBlogDetailByBlogId = async (blogId) => {
     const response = await apiAxios.get(`/blogs/${blogId}`);
+    
+    return response.data;
+};
 
+const getLikeCountByBlogId = async (blogId) => {
+    const response = await apiAxios.get(`/blogs/${blogId}/followers/count`);
+    return response.data;
+};
+
+const getIsLikedByBlogId = async (blogId) => {
+    const response = await apiAxios.get(`/blogs/${blogId}/follow`);
     return response.data;
 };
 
 const likeOrUnlikeBlogAPI = async (isLiked, blogId) => {
-
     if (isLiked) {
         // 관심 해제
-        const response = await apiAxios.delete(`/blogs/${blogId}/likes`);
+        const response = await apiAxios.delete(`/blogs/${blogId}/follow`);
     } else {
         // 관심 등록
-        const response = await apiAxios.post(`/blogs/${blogId}/likes`, null);
+        const response = await apiAxios.post(`/blogs/${blogId}/follow`);
     }
 };
 
 const editIntroduce = async (introduce, blogId) => {
-    
     const body = {
         introduce,
     }
-    const response = await apiAxios.put(`/blogs/${blogId}`, body);
-    return response.data;
+    const response = await apiAxios.patch(`/blogs/${blogId}`, body);
     
+    return response.data;
 }
 
 const saveBoard = async (boardId, formData) => {
@@ -68,4 +77,13 @@ const getBoardDetailWithLike = async (boardId) => {
 }
 
 
-export { getBlogDetailByBlogId, likeOrUnlikeBlogAPI, editIntroduce, saveBoard, fetchBoardDataByBoardId, getBoardDetailWithLike };
+export { 
+    getBlogDetailByBlogId, 
+    likeOrUnlikeBlogAPI, 
+    editIntroduce, 
+    saveBoard, 
+    fetchBoardDataByBoardId, 
+    getBoardDetailWithLike, 
+    getLikeCountByBlogId,
+    getIsLikedByBlogId
+ };
