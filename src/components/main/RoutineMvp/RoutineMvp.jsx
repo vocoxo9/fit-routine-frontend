d1891f7ba72cc50e777ce2be502a37cec84ea5f4
Kim-ilhyeon <kih3915@naver.com>
2025-06-14 01:50:27 +0900

    token값 기준 UI 수정 및 api요청 예외처리 추가

--- src/components/main/RoutineMvp/RoutineMvp.jsx
+++ src/components/main/RoutineMvp/RoutineMvp.jsx
@@ -8,30 +8,36 @@ function RoutineMvp() {
 
     const [mvpRank, setMvpRank] = useState([]);
 
+    const token = localStorage.getItem("accessToken");
+
     useEffect(() => {
         const fetchRoutineMvp = async () => {
-            const fetchMyRank = async () => {
-                const result = await getMyRank();
-                setMyRank(`${result.rank}등 (${result.count}건)`);
-            }
-            await fetchMyRank();
-            
-            const fetchMvpData = async () => {
+            try {
                 const result = await getMvpRank();
                 setMvpRank(result);
+            } catch (error) {
+                console.error("Routine 데이터가 부족합니다.");
             }
-            await fetchMvpData();
+
+            if (token) {
+                try {
+                    const result = await getMyRank();
+                    setMyRank(`${result.rank}등 (${result.count}건)`);
+                } catch (error) {
+                    console.error("등록된 루틴이 없습니다.");
+                }
+            }
         }
         fetchRoutineMvp();
-    },[]);
+    },[token]);
 
     return (
         <>
             <div className={styles.title}>
                 <div>이달의 루틴 MVP</div>
                 <div className={styles.myRank}>
-                    {myRank ?
-                        (<p>나의 순위 : {myRank}</p>) :
+                    {token ?
+                        myRank ? <p>나의 순위 : {myRank}</p> : <p>아직 등록된 루틴이 없습니다.</p> :
                         (<p>나의 순위는 로그인 후 확인이 가능합니다.</p>)
                     }
                 </div>
